38b67f1fc4affaa410ca3f44cd256511ac704e3f
@onurgndz1905 <gunduzonur@gmail.com>
2023-02-28 09:39:35 +0300

    first commit

---
+++ clock.js
@@ -0,0 +1,13 @@
+//prompt ile kullanıcıdan ile iletişim 
+
+const name = prompt("LÜTFEN İSİMİNİZİ GİRİNİZ : ");
+
+//İD İle ismi yazdırmak için iletişim sağla
+
+const deger = document.getElementById("myName");
+
+//İd ile tarihi güncell tutmak için iletişim sağla
+
+const tarih = document.getElementById("myClock");
+
+tarih.innerHTML=Date();
